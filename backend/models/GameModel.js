e86b9d311cc2bfdc38dca969b770d250be03146e
CarlosSmariano <sanchesmarianocarlos@gmail.com>
2025-10-06 16:21:37 -0300

    Armazenando dados em uma array (simulaçao de banco de dados)

---
+++ backend/models/GameModel.js
@@ -0,0 +1,9 @@
+//Simulação do banco de dados da loja
+let games = [
+    {id: 1, name: "Hollow Knight", price: 30.00},
+    {id: 2, name: "Hollow Knight: Silksong", price: 60.00},
+    {id: 4, name: "Ghost of Tsushima", price: 300.00},
+    {id: 3, name: "Ghost of Yotei", price: 400.00},
+];
+
+module.exports = games;
